22f4ab88013bf1a4ce0b79636b22f848616ed67d
Sven Celi <sven.celi@princen-group.com>
2021-10-29 11:05:02 +0200

    first commit

---
+++ src/Themes/FancyWebsite.Theme/Manifest.cs
@@ -0,0 +1,9 @@
+using OrchardCore.DisplayManagement.Manifest;
+
+[assembly: Theme(
+    Name = "FancyWebsite.Theme",
+    Author = "The Orchard Core Team",
+    Website = "https://orchardcore.net",
+    Version = "0.0.1",
+    Description = "FancyWebsite.Theme"
+)]
